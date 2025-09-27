bab92fe064b7bfdcc26350dfd0bb812d977e93ae
Recraz <95542186+Recraz@users.noreply.github.com>
2025-09-27 06:41:36 +0200

    41

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1758947103|30328928394';
+const CACHE_VERSION = '1758948086|31311944375';
 /** @type {string} */
 const CACHE_PREFIX = 'Forty Four-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
